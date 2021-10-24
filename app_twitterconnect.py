884d1f381c747d73b8bb35451a437d02c782da3b
Iván Ortiz Martín <ivan@transgruasortiz.com>
2021-10-24 21:48:09 +0200

    feat: 1st step: Analysis with python blobtext. Init.

--- app_twitterconnect.py
+++ app_twitterconnect.py
@@ -14,6 +14,9 @@ from flask import Flask, jsonify
 import logging
 import yaml
 import tweepy
+from textblob import TextBlob
+# Importing the NaiveBayesAnalyzer classifier from NLTK
+from textblob.sentiments import NaiveBayesAnalyzer
 
 with open("config.yaml", "r") as stream:
     auth_tweeter = yaml.safe_load(stream)
@@ -27,10 +30,41 @@ acces_token_secret = auth_tweeter['acces_token_secret']
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(acces_token, acces_token_secret)
 api = tweepy.API(auth)
+
+corpus_tweets = api.search_tweets('cryptocurrencies', count=5)
+for tweet in corpus_tweets:
+    print(tweet.text)
+
+# Applying the NaiveBayesAnalyzer
+blob_object = TextBlob(tweet.text, analyzer=NaiveBayesAnalyzer())
+# Running sentiment analysis
+analysis = blob_object.sentiment
+print(analysis)
+
+
+# api.available_trends()
+# api.search_tweets("crypto")
+
+
+# # Get n-messages
 # for tweet in tweepy.Cursor(api.search_tweets, q='tweepy').items(10):
 #     print(tweet.text)
 
-api.update_status("Hello world!! this is a new incredible account to post updates about my trading bot results!!!")
+# # Writting direct message
+# api.update_status("Hello world!! this is a new incredible account to post updates about my trading bot results!!!")
 
+# # Requests curl
 # 'curl --request GET 'https://api.twitter.com/2/tweets/search/recent?query=from:twitterdev' --header
 # 'Authorization: Bearer $BEARER_TOKEN'
+
+##########################################################
+# # Con Clases
+# class ApiClass:
+#     def __init__(self, api_key, api_key_secret):
+#         self.auth = tweepy.OAuthHandler(api_key, api_key_secret)
+#         self.auth.set_access_token(acces_token, acces_token_secret)
+#         self.api = tweepy.API(self.auth)
+#
+#     def reload(self):
+#         return self.api
+##########################################################
